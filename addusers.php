361fd215a983bfed76ffb87b4a5924f2bfe2aece
programmer647 <bourne.scm@oundleschool.org.uk>
2023-10-13 10:10:59 +0100

    added header to take you back to users page

--- addusers.php
+++ addusers.php
@@ -1,5 +1,9 @@
 <?php
+
+header('Location: users.php');
 include_once("connection.php");
+
+array_map("htmlspecialchars",$_POST);
 
 switch($_POST["role"]){
 	case "Pupil":
@@ -13,14 +17,16 @@ switch($_POST["role"]){
 		break;
 }
 
-$stmt = $conn->prepare("INSERT INTO TblUser (UserID,Gender,Surname,Forename,Password,House,Year ,Role)VALUES (null,:gender,:surname,:forename,:password,:house,:year,:role)");
 
-$stmt->bindParam(':forename', $_POST[“forename”]);
-$stmt->bindParam(':surname', $_POST[“surname”]);
-$stmt->bindParam(':house', $_POST[“house”]);
-$stmt->bindParam(':year', $_POST[“year”]);
-$stmt->bindParam(':password', $_POST[“passwd”]);
-$stmt->bindParam(':gender', $_POST[“gender”]);
+
+$stmt = $conn->prepare("INSERT INTO TblUsers (UserID,Gender,Surname,Forename,Password,House,Year ,Role)VALUES (null,:gender,:surname,:forename,:password,:house,:year,:role)");
+
+$stmt->bindParam(':forename', $_POST["forename"]);
+$stmt->bindParam(':surname', $_POST["surname"]);
+$stmt->bindParam(':house', $_POST["house"]);
+$stmt->bindParam(':year', $_POST["year"]);
+$stmt->bindParam(':password', $_POST["passwd"]);
+$stmt->bindParam(':gender', $_POST["gender"]);
 $stmt->bindParam(':role', $role);
 $stmt->execute();
 $conn=null;
